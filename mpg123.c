7eaf26caba0a6a3cef99dad4bc8497fb8cf14ff8
Matthias Georgi <matthias@soundcloud.com>
2014-02-02 17:17:18 +0100

    fix indent

--- mpg123.c
+++ mpg123.c
@@ -4,31 +4,31 @@
 #include "soundcloud3000.h"
 
 mpg123_handle *mpg_open_file(const char *filename) {
-  int err = MPG123_OK;
-  mpg123_handle *mh;
-  long rate;
-  int channels, encoding;
+    int err = MPG123_OK;
+    mpg123_handle *mh;
+    long rate;
+    int channels, encoding;
 
-  if ((mh = mpg123_new(NULL, &err)) == NULL) {
-    fprintf(stderr, "%s", mpg123_plain_strerror(err));
-    return NULL;
-  }
+    if ((mh = mpg123_new(NULL, &err)) == NULL) {
+        fprintf(stderr, "%s", mpg123_plain_strerror(err));
+        return NULL;
+    }
 
-  mpg123_param(mh, MPG123_ADD_FLAGS, MPG123_FORCE_FLOAT, 0.);
+    mpg123_param(mh, MPG123_ADD_FLAGS, MPG123_FORCE_FLOAT, 0.);
 
-  if (mpg123_open(mh, filename) != MPG123_OK ||
-      mpg123_getformat(mh, &rate, &channels, &encoding) != MPG123_OK) {
-    fprintf(stderr, "%s", mpg123_strerror(mh));
-    return NULL;
-  }
+    if (mpg123_open(mh, filename) != MPG123_OK ||
+        mpg123_getformat(mh, &rate, &channels, &encoding) != MPG123_OK) {
+        fprintf(stderr, "%s", mpg123_strerror(mh));
+        return NULL;
+    }
 
-  if (encoding != MPG123_ENC_FLOAT_32) {
-    fprintf(stderr, "bad encoding");
-    return NULL;
-  }
+    if (encoding != MPG123_ENC_FLOAT_32) {
+        fprintf(stderr, "bad encoding");
+        return NULL;
+    }
 
-  mpg123_format_none(mh);
-  mpg123_format(mh, rate, channels, encoding);
+    mpg123_format_none(mh);
+    mpg123_format(mh, rate, channels, encoding);
 
-  return mh;
+    return mh;
 }
